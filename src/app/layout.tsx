12cc63a893904984fbfffac678afe94a27c1110e
Dominique Degottex <75885483+DomiDex@users.noreply.github.com>
2025-02-01 13:12:12 +0700

    Add theme support and enhance landing page with dark mode and features

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import './globals.css';
 import { siteFont } from '@/fonts';
+import { ThemeProvider } from '@/features/theme/components/theme-provider';
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -13,8 +14,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang='en'>
-      <body className={`${siteFont.variable} antialiased`}>{children}</body>
+    <html lang='en' suppressHydrationWarning>
+      <body className={`${siteFont.variable} antialiased`}>
+        <ThemeProvider>{children}</ThemeProvider>
+      </body>
     </html>
   );
 }
